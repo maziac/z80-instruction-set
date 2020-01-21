8ef8371f840e9ba2268b1396efda55dd881ce289
maziac <thomas.busse@gmx.net>
2020-01-21 22:44:38 +0100

    Legend added.

--- src/HoverUtils.ts
+++ src/HoverUtils.ts
@@ -31,3 +31,46 @@ export function extractInstruction(line: string, index: number): string {
 
     return rawInstruction;
 }
+
+
+/**
+ * Creates a legend for a given description.
+ * I.e. adds only the parts that are contained in the instruction.
+ * @param instruction E.g. "BIT b,(IX+o)"
+ * @returns E.g "b=0-7, o=-128 to 127)". 'undefined' if no legend necessary.
+ */
+export function getLegend(instruction: string): string {
+    const legendArr = new Array<string>();
+    // Check for a 'b'
+    if (instruction.indexOf('b') >= 0)
+        legendArr.push('b=0-7 (bit)');
+    // Check for a 'n'
+    if (instruction.indexOf('n') >= 0)
+        legendArr.push('n=0-255');
+    // Check for a 'nn'
+    if (instruction.indexOf('nn') >= 0)
+        legendArr.push('nn=0-65535');
+    // Check for a 'o'
+    if (instruction.indexOf('o') >= 0)
+        legendArr.push('o=-128 to 127');
+    // Check for a 'r'
+    if (instruction.indexOf('r') >= 0)
+        legendArr.push('r: B=0 C=1 D=2 E=3 H=4 L=5 A=7');
+    // Check for a 'p'
+    if (instruction.indexOf('p') >= 0)
+        legendArr.push('p: B=0 C=1 D=2 E=3 IXh=4 IXl=5 A=7');
+    // Check for a 'q'
+    if (instruction.indexOf('q') >= 0)
+        legendArr.push('q: B=0 C=1 D=2 E=3 IYh=4 IYl=5 A=7');
+
+    // Check if empty
+    if (legendArr.length == 0)
+        return undefined;
+
+    // Create string from array
+    const legend = legendArr.join(', ');
+
+    TEST
+
+    return legend;
+}
