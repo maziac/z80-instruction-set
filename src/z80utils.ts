ecde1183cb5acfeb07b7df5c1a4ac18179c4a003
maziac <thomas.busse@gmx.net>
2021-10-24 16:55:29 +0200

    Removed unused files.

--- src/z80utils.ts
+++ src/z80utils.ts
@@ -5,17 +5,6 @@ export function parseTimings(s: string): number[] {
     return ss.length === 1 ? [t0, t0] : [t0, parseInt(ss[1])];
 }
 
-export function formatTiming(t: number[]): string {
-    return t[0] === t[1] ? t[0].toString() : t[0] + "/" + t[1];
-}
-
-export function extractInstructionFrom(rawLine: string): string | undefined {
-    // Removes surrounding label, whitespace and/or comments
-    const line = rawLine.replace(/(^\s*\S+:)|((;|\/\/).*$)/, "").trim();
-    // Simplifies whitespace and converts to uppercase
-    return line.length === 0 ? undefined : line.replace(/\s+/, " ").toUpperCase();
-}
-
 export function extractMnemonicOf(s: string): string {
     const i = s.indexOf(" ");
     return i === -1 ? s : s.substring(0, i);
